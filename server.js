7080be63dfd7535808b68d51fbcf8c88104a17ec
angelia collins <angiecollins09@gmail.com>
2020-12-18 16:52:57 -0700

    attempting to set up api route

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var path = require("path");
 var http = require("http");
 var fs = require("fs");
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,6 +17,15 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "notes.html"));
 });
 
+//api routes
+app.get("/api/notes", function(req, res) {
+    fs.readFilefs.readFile(__dirname + "/notes.html", function(err, data) {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      });
+    return res.json(characters);
+  });
 
 
 app.listen(PORT, function () {
